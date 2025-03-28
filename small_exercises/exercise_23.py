556ad9be98c8ba3516cd478c67c059cdbaff40b5
jleftync <justinstark79@gmail.com>
2025-03-28 18:08:29 +0000

    working

---
+++ small_exercises/exercise_23.py
@@ -0,0 +1,9 @@
+def merge_sets(lst1, lst2):
+    out_set = lst1 + lst2
+    out_set = set(out_set)
+    return out_set
+
+
+list1 = [3, 5, 7, 9]
+list2 = [5, 7, 11, 13]
+print(merge_sets(list1, list2) == {3, 5, 7, 9, 11, 13})
