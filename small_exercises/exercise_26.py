c9663d260d7208e561af2f9ab01e5644c557bae4
jleftync <justinstark79@gmail.com>
2025-03-30 16:20:41 +0000

    works

--- small_exercises/exercise_26.py
+++ small_exercises/exercise_26.py
@@ -1,4 +1,5 @@
 def double_char(char):
+    
     z = char + char
     return z
 
@@ -12,3 +13,16 @@ def repeater(strng):
 print(repeater('Hello') == "HHeelllloo")              # True
 print(repeater('Good job!') == "GGoooodd  jjoobb!!")  # True
 print(repeater('') == "")    
+
+
+def repeater(strng):
+    y = ""
+    for x in range(len(strng)):
+        y += (strng[x] * 2)
+    return y
+
+print(repeater('Hello') == "HHeelllloo")              # True
+print(repeater('Good job!') == "GGoooodd  jjoobb!!")  # True
+print(repeater('') == "")
+def repeater(string):
+    return ''.join([char * 2 for char in string])
