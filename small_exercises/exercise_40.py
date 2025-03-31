2b8269c8c7bbdde4552e08b5ae39b2d73c4e51ea
jleftync <justinstark79@gmail.com>
2025-03-31 23:36:46 +0000

    works

---
+++ small_exercises/exercise_40.py
@@ -0,0 +1,24 @@
+def transactions_for(inpt_1, in_lst):
+    return [in_lst[x] for x in range(len(in_lst)) if inpt_1 in in_lst[x].values()]
+
+
+
+transactions = [
+    {"id": 101, "movement": 'in',  "quantity":  5},
+    {"id": 105, "movement": 'in',  "quantity": 10},
+    {"id": 102, "movement": 'out', "quantity": 17},
+    {"id": 101, "movement": 'in',  "quantity": 12},
+    {"id": 103, "movement": 'out', "quantity": 20},
+    {"id": 102, "movement": 'out', "quantity": 15},
+    {"id": 105, "movement": 'in',  "quantity": 25},
+    {"id": 101, "movement": 'out', "quantity": 18},
+    {"id": 102, "movement": 'in',  "quantity": 22},
+    {"id": 103, "movement": 'out', "quantity": 15},
+]
+
+print(transactions_for(101, transactions) ==
+      [
+          {"id": 101, "movement": "in",  "quantity":  5},
+          {"id": 101, "movement": "in",  "quantity": 12},
+          {"id": 101, "movement": "out", "quantity": 18},
+      ]) # True
