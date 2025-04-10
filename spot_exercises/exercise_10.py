fa433a264cc787e0aa5e9ecd53241abeb99a6c6b
jleftync <justinstark79@gmail.com>
2025-04-10 13:13:28 +0000

    done

--- spot_exercises/exercise_10.py
+++ spot_exercises/exercise_10.py
@@ -1,26 +1,11 @@
-def top_3_words(i_string):
-    i_list = i_string.split()
-    n_list = [x.split("-") for x in i_list]
-    o_list = [y for z in n_list for y in z]
-    
-    p_list = []
-    for d in o_list:
-        a = ""
-        for e in d:
-            if e.isalpha():
-                a += e
-        if a:
-            p_list.append(a)
-    
-    o_set = {(p_list.count(e), e) for e in p_list}
-    o_list = list(o_set)
-    o_list.sort()
-    o_list.reverse()
-    if len(o_list) < 3:
-        print([s[1] for s in o_list])
-        return [s[1] for s in o_list]
-    else:
-        
-        return [s[1] for s in o_list[0: 3]]
+def domain_name(i_str):
+    f_list = i_str.split("/")
+    s_list = [x.split(".") for x in f_list]
+    t_list = [b for a in s_list for b in a]
+    for idx, word in enumerate(t_list):
+        if word == 'com':
+            return t_list[idx-1]
 
-top_3_words("n a village of La Mancha, the name of which I have no desire to call to")
+
+domain_name("http://github.com/carbonfive/raygun") # should return "github"
+domain_name("https://www.cnet.com") # should return "cnet"
