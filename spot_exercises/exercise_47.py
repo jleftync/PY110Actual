ffaeeeb9de2692049bf95aeeca7ec6523221c518
jleftync <justinstark79@gmail.com>
2025-04-16 13:02:28 +0000

    working

--- spot_exercises/exercise_47.py
+++ spot_exercises/exercise_47.py
@@ -1,5 +1,10 @@
 def find_missing_letter(i_list):
-    pass
+    f_list = [x for x in range(ord(i_list[0]), ord(i_list[-1]) + 1)]
+    s_list = [chr(y) for y in f_list]
+    for z in s_list:
+        if z not in i_list:
+            print(z)
+            return z
 
 
 find_missing_letter(['a','b','c','d','f'])
