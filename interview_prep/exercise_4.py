7351a40bc70f5c04dfae93f923c68f0ed4cda2eb
jleftync <justinstark79@gmail.com>
2025-04-18 17:56:23 +0000

    next

--- interview_prep/exercise_4.py
+++ interview_prep/exercise_4.py
@@ -8,6 +8,14 @@ def closest_numbers(input_list):
     y = (srt_list[x], srt_list[x + 1]) if input_list.index(srt_list[x]) < input_list.index(srt_list[x+1]) else (srt_list[x+1], srt_list[x])
     print(y)
 
+    srt = sorted(input_list)
+    pairs = list(zip(srt, srt[1:]))
+    closest_pair = min(pairs, key=lambda pair: abs(pair[1] - pair[0]))
+    
+    # Reorder to match original input
+    a, b = closest_pair
+    return (a, b) if input_list.index(a) < input_list.index(b) else (b, a)
+
 
 print(closest_numbers([5, 25, 15, 11, 20]) == (15, 11))
 print(closest_numbers([19, 25, 32, 4, 27, 16]) == (25, 27))
