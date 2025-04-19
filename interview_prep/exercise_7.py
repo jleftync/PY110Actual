c711321463d5d6e77f3dabf0ec8796314b1a716f
jleftync <justinstark79@gmail.com>
2025-04-19 14:26:13 +0000

    working

---
+++ interview_prep/exercise_7.py
@@ -0,0 +1,17 @@
+def pairs(in_list):
+    if in_list == False:
+        return 0
+    num_set = set(in_list)
+    pair_list = [in_list.count(c)//2 for c in num_set]
+    return sum(pair_list)
+
+
+
+
+print(pairs([3, 1, 4, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7]) == 3)
+print(pairs([2, 7, 1, 8, 2, 8, 1, 8, 2, 8, 4]) == 4)
+print(pairs([]) == 0)
+print(pairs([23]) == 0)
+print(pairs([997, 997]) == 1)
+print(pairs([32, 32, 32]) == 1)
+print(pairs([7, 7, 7, 7, 7, 7, 7]) == 3)
