cf41a53b7b7de86254b934c238a347af6aea5c0a
jleftync <justinstark79@gmail.com>
2025-04-20 07:35:04 +0000

    working

---
+++ interview_prep/exercise_9.py
@@ -0,0 +1,13 @@
+def count_substrings(fi_string, se_string):
+    out_list = fi_string.replace(se_string, "*")
+    return out_list.count("*")
+
+print(count_substrings('babab', 'bab') == 1)
+print(count_substrings('babab', 'ba') == 2)
+print(count_substrings('babab', 'b') == 3)
+print(count_substrings('babab', 'x') == 0)
+print(count_substrings('babab', 'x') == 0)
+print(count_substrings('', 'x') == 0)
+print(count_substrings('bbbaabbbbaab', 'baab') == 2)
+print(count_substrings('bbbaabbbbaab', 'bbaab') == 2)
+print(count_substrings('bbbaabbbbaabb', 'bbbaabb') == 1)
