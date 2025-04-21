1d548bcda0b3484507afabc07a67974dacbaae86
jleftync <justinstark79@gmail.com>
2025-04-21 13:03:09 +0000

    working

---
+++ interview_prep/exercise_16.py
@@ -0,0 +1,16 @@
+def distinct_multiples(f_string):
+    f_list = list(f_string.lower())
+    mult_set = set([char for char in f_list if f_list.count(char) > 1])
+    return len(mult_set)
+
+
+
+
+print(distinct_multiples('xyz') == 0)               # (none)
+print(distinct_multiples('xxyypzzr') == 3)          # x, y, z
+print(distinct_multiples('xXyYpzZr') == 3)          # x, y, z
+print(distinct_multiples('unununium') == 2)         # u, n
+print(distinct_multiples('multiplicity') == 3)      # l, t, i
+print(distinct_multiples('7657') == 1)              # 7
+print(distinct_multiples('3141592653589793') == 4)  # 3, 1, 5, 9
+print(distinct_multiples('2718281828459045') == 5)  # 2, 1, 8, 4, 5
---
+++ interview_prep/exercise_17.py
@@ -0,0 +1,7 @@
+print(nearest_prime_sum([1, 2, 3]) == 1)        # Nearest prime to 6 is 7
+print(nearest_prime_sum([5, 2]) == 4)           # Nearest prime to 7 is 11
+print(nearest_prime_sum([1, 1, 1]) == 2)        # Nearest prime to 3 is 5
+print(nearest_prime_sum([2, 12, 8, 4, 6]) == 5) # Nearest prime to 32 is 37
+
+# Nearest prime to 163 is 167
+print(nearest_prime_sum([50, 39, 49, 6, 17, 2]) == 4)
