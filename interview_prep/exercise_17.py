0c41f47f0631d1ec7a90bf13c9bb7a7db75b5fc9
jleftync <justinstark79@gmail.com>
2025-04-21 14:26:14 +0000

    working

--- interview_prep/exercise_17.py
+++ interview_prep/exercise_17.py
@@ -1,3 +1,27 @@
+
+
+def nearest_prime_sum(i_list):
+    si = sum(i_list)
+    counter = 1
+    while True:
+        
+        so = si + counter
+        for x in range(2, int(so ** .5) + 1):
+            if so % 3 == 0:
+                counter += 1
+                break
+            if so % 3 ==0:
+                counter += 1
+                break
+            if so % x == 0:
+                counter += 1
+                break
+            
+        else:
+            return so - si
+
+
+
 print(nearest_prime_sum([1, 2, 3]) == 1)        # Nearest prime to 6 is 7
 print(nearest_prime_sum([5, 2]) == 4)           # Nearest prime to 7 is 11
 print(nearest_prime_sum([1, 1, 1]) == 2)        # Nearest prime to 3 is 5
